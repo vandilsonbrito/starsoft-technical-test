da611ac752ca7583ab4eae34f08f599325515f89
vandilsonbrito <vandilson25@outlook.com>
2024-10-26 16:16:24 -0300

    Feat: create 'Header' component

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+        <img src="./logo.svg" alt="Logo" />
+        <img src="./bag-icon.svg" alt="Bag icon" />
+    </header>
+  )
+}
