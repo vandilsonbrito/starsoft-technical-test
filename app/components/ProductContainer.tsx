2226833bcec89ed1814345641b8d890a0dfba90e
vandilsonbrito <vandilson25@outlook.com>
2024-10-28 10:43:05 -0300

    Feat: create 'handleBuyButton' function and check if item is in the cart to display button's text

--- app/components/ProductContainer.tsx
+++ app/components/ProductContainer.tsx
@@ -1,25 +1,59 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { addToCart } from '@/utils/redux/actions/cartAction';
+import { useDispatch, useSelector } from 'react-redux';
+import { ItemsFromData   } from '@/utils/types/types';
+import { RootState } from '@/utils/redux/store';
 
-export default function ProductContainer({ imgSrc, name, description, price }: { imgSrc: string, name: string, description: string, price: number }) {
-  return (
-    <div className="product-container">
-        <div className="item-img-wrapper">
-            <Image src={imgSrc} alt="Item" width={300} height={300} />
-        </div>
-        <div className="item-info-container">
-            <div className="item-info-wrapper">
-                <p className="item-title">{name}</p>
-                <p className="item-description">{description}</p>
-            </div>
+export default function ProductContainer({ item }: { item: ItemsFromData }) {
+    
+    const dispatch = useDispatch();
+    const cart = useSelector((state: RootState) => state.cartItems);
+    const [isItItemInTheCart, setIsItItemInTheCart] = useState<boolean>(false);
 
-            <div className="item-price-wrapper">
-                <img src="./ellipse-icon.svg" alt="ETH icon" />
-                <p>{`${price} ETH`}</p>
+    useEffect(() => {
+        if(cart.cartItems) {
+            console.log("cart.cartItems", cart.cartItems)
+            const isTrue = cart.cartItems.some(product => product.id === item.id)
+            if(isTrue) {
+                setIsItItemInTheCart(true);
+            }
+            else {
+                setIsItItemInTheCart(false);
+            }
+        }
+    }, [cart, item.id])
+
+
+    const handleBuyButton = () => {
+        if(!isItItemInTheCart) {
+            dispatch(addToCart(item))
+        }
+    }
+    
+    return (
+        <div className="product-container">
+            <div className="item-img-wrapper">
+                <Image src={item.image} alt="Item" width={300} height={300} />
             </div>
-            <button className="generic-style-btn">Comprar</button>
+            <div className="item-info-container">
+                <div className="item-info-wrapper">
+                    <p className="item-title">{item.name}</p>
+                    <p className="item-description">{item.description}</p>
+                </div>
+
+                <div className="item-price-wrapper">
+                    <img src="./ellipse-icon.svg" alt="ETH icon" />
+                    <p>{`${item.price} ETH`}</p>
+                </div>
+                <button 
+                    onClick={() => handleBuyButton()}
+                    className="generic-style-btn desktop-hover-effect"
+                    >{ isItItemInTheCart ? 'Adicionado ao carrinho' : 'Comprar' }
+                </button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
