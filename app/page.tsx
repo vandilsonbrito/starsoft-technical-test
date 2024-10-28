72b8b8209e6329aa37c688b3b53a87414ff60e6d
vandilsonbrito <vandilson25@outlook.com>
2024-10-28 10:13:34 -0300

    Feat: add 'OverlayCheckout' component

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import DisplayingProducts from "./components/DisplayingProducts";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import LoadMoreBtn from "./components/LoadMoreBtn";
+import OverlayCheckout from "./components/OverlayCheckout";
 
 export default function Home() {
 
     return (
       <main className='home-page'>
         <Header/>
+        <OverlayCheckout/>
         <section className='home-page-section'>
             <div className='products-wrapper'>
                <DisplayingProducts/>
