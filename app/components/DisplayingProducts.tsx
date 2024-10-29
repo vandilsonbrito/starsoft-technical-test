380bb806bcb5b7a4bc6d566b95e8e996cc319704
vandilsonbrito <vandilson25@outlook.com>
2024-10-29 02:23:02 -0300

    Feat: add api error check

--- app/components/DisplayingProducts.tsx
+++ app/components/DisplayingProducts.tsx
@@ -4,22 +4,32 @@ import { useProductData } from '@/utils/hooks/useProductsData';
 import ProductContainer from './ProductContainer';
 import { ItemsFromData } from '@/utils/types/types';
 import ProductFallbackContainer from './ProductFallbackContainer';
-import { useSelector  } from 'react-redux';
+import { useDispatch, useSelector  } from 'react-redux';
 import { RootState } from '../../utils/redux/store'; 
 import FadeInOnScroll from './ItemAnimationEffect';
+import { setIsThereData } from '@/utils/redux/slices/isThereAPIdata';
 
 export default function DisplayingProducts() {
 
+    const dispatch = useDispatch();
     const limitValue = useSelector((state: RootState) => state.limit.value);
-    const { data } = useProductData(limitValue);
+    const { data, error } = useProductData(limitValue);
     const [items, setItems] = useState<ItemsFromData[] | []>([])
 
     useEffect(() => {
         console.log("Data", data)
         if(data) {
+            dispatch(setIsThereData(true));
             setItems(data.data)
         }
-      }, [data]);
+    }, [data]);
+
+    useEffect(() => {
+        if(error) {
+            dispatch(setIsThereData(false));
+        }
+    }, [error]);
+
 
     return (
         <>
